272af96908f0b87a98b193f280c4407623316a80
Loksta8 <psycho_sk8er_dude_69@hotmail.com>
2025-06-08 17:40:29 -0600

    - test

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// hopefully last test
+// test
 #include <SFML/Graphics.hpp>
 #include "Ant.hpp"
 #include "Colony.hpp"
